992a086a0337e89e8a0201fb1dc0352ed2d6c9cc
khemistBoy <stegert.johansson@gmail.com>
2025-04-29 11:49:52 +0300

    Update keemia_bot_nalja_pärast.py

--- keemia_bot_nalja_pärast.py
+++ keemia_bot_nalja_pärast.py
@@ -1,8 +1,8 @@
 import discord
 from discord.ext import commands
 
-BOT_TOKEN = 
-CHANNEL_ID = 1366509155455602782
+TOKEN = os.getenv("BOT_TOKEN")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 
 intents = discord.Intents.default()
 intents.message_content = True
